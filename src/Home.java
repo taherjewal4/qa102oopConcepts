424b82fdcdc1ac1b6d7256a129f64814f192f7cf
Jewal <taherjuwel3262@gmail.com>
2021-11-28 11:37:10 -0500

    first commit for Home and students class

---
+++ src/Home.java
@@ -0,0 +1,19 @@
+public class Home {
+
+
+    // static
+    // non static
+
+
+    static String name;
+    static String address;
+
+
+    public static void getName(){
+        System.out.println(name);
+    }
+
+    public void getAddress(){
+        System.out.println(address);
+    }
+}
---
+++ src/Students.java
@@ -0,0 +1,45 @@
+public class Students { //box
+
+    private String name;
+    private int id;
+    // Class = is A blueprint of an object
+    // Object = Object is an instance of a class
+
+    public Students() {
+
+    }
+
+    public Students(String name) { //"david
+        this.name = name;
+    }
+
+    public Students(String name, int id) { //"adam
+        this.name = name;
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+//    public String studentName(String name4) {
+//        String name1 = name;
+//        return name1;
+//    }
+//
+//    public int studentID() {
+//        System.out.println(name);
+//        return id;
+//    }
+//
+//    public void studentSection() {
+//        System.out.println("This is a student Section method");
+//    }
+}
+
+
